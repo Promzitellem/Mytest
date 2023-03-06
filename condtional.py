bf967a9931ea794da6fb55450d9ac7001fdd8291
Promzitellem <126804343+Promzitellem@users.noreply.github.com>
2023-03-06 16:26:24 -0500

    Add files via upload

---
+++ condtional.py
@@ -0,0 +1,13 @@
+available_drinks = ['coffee', 'tea', 'water', 'orange juice']
+
+if 'coffee' in available_drinks:
+  print('Drink is available.')
+  
+if 'milk' not in available_drinks:
+   print('Milk is out of stock.')
+   
+had_coffee_already = False
+is_tired = True
+
+if had_coffee_already == False or is_tired == True:
+   print('Agent J is tired and need coffee.')
